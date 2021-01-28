73db6f07ca3cc05848f7f1a635e464fa6778177d
Irene Asay <irene.hsu.asay@gmail.com>
2021-01-28 15:46:21 +0800

    added route to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./container/About";
+import Contact from "./container/Contact"
+import Portfolio from "./container/Portfolio";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={About} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/contact" component={Contact} />
-      <Route exact path="/portfolio" component={Portfolio} />
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route component={NoMatch} />
+          <Footer />
+        </Switch>
+      </div>
     </Router>
   );
 }
