88a95aeba58535b3aa29d9831ee6b193c6ad9aaa
Irene Asay <irene.hsu.asay@gmail.com>
2021-01-30 20:40:15 +0800

    added navbar with Bootstrap styles to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Header extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+const Header = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark nav-font">
+        <span className="navbar-brand mb-0 h1">Irene Asay</span>
+        <div className="ml-auto">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link active" href="index.html">
+                About <span className="sr-only">(current)</span>
+              </a>
+              <a className="nav-link" href="portfolio.html">
+                Portfolio
+              </a>
+              <a className="nav-link" href="contact.html">
+                Contact
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Header;
