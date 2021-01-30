7e0f5252870e88df44026bfa07511ecb73cf94bd
Irene Asay <irene.hsu.asay@gmail.com>
2021-01-30 21:16:17 +0800

    added Portfolio content  with image paths to screenshots

--- src/container/Portfolio/Portfolio.jsx
+++ src/container/Portfolio/Portfolio.jsx
@@ -1,9 +1,123 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Portfolio extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+const Portfolio = () => {
+  return (
+    <div className="container col-lg-7 col-md-6 col-xs-10">
+      <div className="row p-2">
+        <div className="col-lg-12 col-md-12 col-xs-12">
+          <h1>Portfolio</h1>
+          <div className="row">
+            <div className="col-lg-6 col-md-12 col-xs-12 mt-3">
+              <a
+                href="https://github.com/rgl10d/conference-room-booker"
+                target="_blank"
+              >
+                <img
+                  src="/public/images/the-reserve-screenshot.png"
+                  width="320"
+                  height="200"
+                  alt="the-reserve-screenshot"
+                  className="img=fluid pr-0"
+                />
+              </a>
+            </div>
+            <div className="col-lg-6 col-md-12 col-xs-12 mt-3">
+              <a
+                href="https://github.com/IreneAsay/workout-tracker"
+                target="_blank"
+              >
+                <img
+                  src="/public/images/workout-tracker_screenshot.png"
+                  width="320"
+                  height="200"
+                  alt="workout-tracker_screenshot"
+                  className="img=fluid pr-0"
+                />
+              </a>
+            </div>
+            <div className="col-lg-6 col-md-12 col-xs-12 mt-3">
+              <a href="https://github.com/IreneAsay/burger" target="_blank">
+                <img
+                  src="/public/images/BurgerScreenshot.png"
+                  width="320"
+                  height="200"
+                  alt="burger-app_screenshot"
+                  className="img=fluid pr-0"
+                />
+              </a>
+            </div>
+            <div className="col-lg-6 col-md-12 col-xs-12 mt-3">
+              <a href="https://github.com/IreneAsay/project-1" target="_blank">
+                <img
+                  src="/public/images/NTT_screenshot.png"
+                  width="320"
+                  height="200"
+                  alt="NetTimeTraveler_screenshot"
+                  className="img=fluid pr-0"
+                />
+              </a>
+            </div>
+            <div className="col-lg-6 col-md-12 col-xs-12 mt-3">
+              <a
+                href="hhttps://github.com/IreneAsay/daily-planner"
+                target="_blank"
+              >
+                <img
+                  src="/public/images/daily_planner_screenshot.png"
+                  width="320"
+                  height="200"
+                  alt="daily-planner_screenshot"
+                  className="img=fluid pr-0"
+                />
+              </a>
+            </div>
+            <div className="col-lg-6 col-md-12 col-xs-12 mt-3">
+              <a
+                href="https://github.com/IreneAsay/password-generator"
+                target="_blank"
+              >
+                <img
+                  src="/public/images/password_generator_screenshot.png"
+                  width="320"
+                  height="200"
+                  alt="password-generator_screenshot"
+                  className="img=fluid pr-0"
+                />
+              </a>
+            </div>
+            <div className="col-lg-6 col-md-12 col-xs-12 mt-3">
+              <a
+                href="https://github.com/IreneAsay/note-taker-app"
+                target="_blank"
+              >
+                <img
+                  src="/public/images/note-taker_screenshot.png"
+                  width="320"
+                  height="200"
+                  alt="note-taker_screenshot"
+                  className="img=fluid pr-0"
+                />
+              </a>
+            </div>
+            <div className="col-lg-6 col-md-12 col-xs-12 mt-3">
+              <a
+                href="https://github.com/IreneAsay/weather-dashboard-server-side-api"
+                target="_blank"
+              >
+                <img
+                  src="/public/images/Weather_screenshot.png"
+                  width="320"
+                  height="200"
+                  alt="weather-api_screenshot"
+                  className="img=fluid pr-0"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Portfolio;
