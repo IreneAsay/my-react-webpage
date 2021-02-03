9dff3a44afd22a976c9c30f34df87958496a054d
Irene Asay <irene.hsu.asay@gmail.com>
2021-02-03 13:36:31 +0800

    updated img paths in the background rul

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App" style={{ backgroundImage: "url(/images/website_background.jpeg)" }}>
+    <div className="App" style={{ backgroundImage: "url(./images/website_background.jpeg)" }}>
       <Router>
         <Header />
         <Switch>
