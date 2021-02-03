a5e01a516759f0a4dc8dfc3075432f831984d6fb
Irene Asay <irene.hsu.asay@gmail.com>
2021-02-04 07:00:51 +0800

    updated router and image path to fix impage displying issue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import About from "./container/About/About";
-import Contact from "./container/Contact/Contact"
+import Contact from "./container/Contact/Contact";
 import Portfolio from "./container/Portfolio/Portfolio";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 // import Project from "./components/Project";
 
-
 function App() {
   return (
-    <div className="App" style={{ backgroundImage: "url(./images/website_background.jpeg)" }}>
-      <Router>
+    <div
+      className="App"
+      style={{ backgroundImage: "url(./images/website_background.jpeg)" }}
+    >
+      <Router basename={process.env.PUBLIC_URL}>
         <Header />
         <Switch>
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
           <Route path="/portfolio" component={Portfolio} />
-          <Route path="/" component={About} />
+          <Redirect to="/about" />
         </Switch>
         <Footer />
       </Router>
